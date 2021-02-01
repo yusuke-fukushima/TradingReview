f4ad0570204950c8391e102fb764df61f94d03b3
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-02-01 07:51:47 +0000

    bootstrapの導入

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -2,6 +2,6 @@ class Customers::SessionsController < Devise::SessionsController
   def new_guest
     customer = Customer.guest
     sign_in customer
-    redirect_to root_path, 
+    redirect_to root_path
   end
 end
