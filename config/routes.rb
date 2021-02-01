06a881d040811d7459fefb46e5b2f182a922d900
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-02-01 14:58:56 +0000

    管理者ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+  
+  devise_scope :admin do
+    post 'admins/guest_sign_in', to: 'admins/sessions#new_guest'
+  end
+  
   devise_for :customers
+  
   devise_scope :customer do
     post 'customers/guest_sign_in', to: 'customers/sessions#new_guest'
   end
+  
   root to: 'homes#top'
+
+  namespace :public do
+  end
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
 end
