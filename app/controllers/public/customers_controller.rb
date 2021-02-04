ef846f7b1ffe954b6919edbc6b335862da2da3c5
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-02-04 05:40:31 +0000

    顧客会員ページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,11 +2,25 @@ class Public::CustomersController < ApplicationController
 before_action :authenticate_customer!
   
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to public_customer_path(@customer.id)
+    else
+      render "edit"
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:name, :email, :postal_code, :address)
   end
 end
