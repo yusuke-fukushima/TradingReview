331fe85024f3cc3803c5fc622edaf88198fb1e17
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-02-04 04:53:13 +0000

    顧客レビュー画面の修正

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,10 +1,13 @@
 class Public::ReviewsController < ApplicationController
   def new
+    @reviews = Review.new
   end
   
   def confirm
+    @review = Review.new
   end
   
   def complete
+    
   end
 end
