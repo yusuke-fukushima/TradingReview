46ae1718ef07d1138744a4168abafa119ca8d46c
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-02-19 06:11:05 +0000

    Aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
 
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
 
   namespace :public do
     resources :customers, only: [:show, :edit, :update]
