3b1cc37e3598430e8cbb243aa1fe62fe5dd427a0
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-03-14 02:13:04 +0000

    使い方、概要のページ削除

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   end
 
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about'
 
   namespace :public do
     resources :customers, only: [:show, :edit, :update]
