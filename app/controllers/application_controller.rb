74a2d5d71bc1789a6439324bb9f6930e8843f35c
yusuke-fukushima <usk.fshima.1994@gmail.com>
2021-04-04 02:58:57 +0000

    strong parameterにgender,ageカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:gender, :age, :name, :email, :postal_code, :address])
   end
 end
